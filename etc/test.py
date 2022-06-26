a53f89a2ee78750e2fb32511d2c8d38252da1d3f
IKU-Trader <docs910@gmail.com>
2022-06-26 16:07:39 +0900

    update

---
+++ etc/test.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 26 12:19:58 2022
+
+@author: docs9
+"""
+
+import sys
+from PyQt5 import QtWidgets
+import numpy as np
+import pyqtgraph as pg
+from PyQt5.QtWidgets import QApplication, QGraphicsScene, QMainWindow
+from CandlePlotWidget import CandlePlotWidget
+import FinanceDataReader as fdr
+from gui_test1 import Ui_MainWindow
+
+
+def timestampArray(df):
+    array = []
+    for i in range(len(df)):
+        index = df.index[i]
+        array.append(index.timestamp())
+    return array
+
+class Window(QtWidgets.QMainWindow, Ui_MainWindow):   
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setupUi(self)
+        
+        scene = QGraphicsScene()
+        self.graphicsView_LT.setScene(scene)
+        self.candle_plot = CandlePlotWidget()
+        proxy_widget = scene.addWidget(self.candle_plot)
+        
+        self.pushButton.clicked.connect(self.plot)
+
+
+    def plot(self):
+        print('clicked')
+        market = 'AAPL'
+        df = fdr.DataReader(market, '2020')
+        t = timestampArray(df)
+        o = list(df['Open'])
+        h = list(df['High'])
+        l = list(df['Low'])
+        c = list(df['Close'])    
+        self.candle_plot.setTitle(market)
+        self.candle_plot.draw([t, o, h, l, c])
+        
+        
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
